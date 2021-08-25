ad3422c7a2348b995da10fbd5a809b830bb38f7b
Kristian Cho <krischo@umich.edu>
2021-08-25 00:04:18 -0400

    Refactor and organize firebase backend code

---
+++ timbre-functions/functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apiKey: "AIzaSyBPw-WEgK-z0771uh278wgryWipgT3wFDk",
+    authDomain: "timbre-33708.firebaseapp.com",
+    databaseURL: "https://timbre-33708-default-rtdb.firebaseio.com",
+    projectId: "timbre-33708",
+    storageBucket: "timbre-33708.appspot.com",
+    messagingSenderId: "864921106215",
+    appId: "1:864921106215:web:2fddc5aa35de84a0a51b02",
+    measurementId: "G-DS9Y0ZL0CX"
+};
