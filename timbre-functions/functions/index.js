6aedf81afff5e5c182e73932eb46813998d6d02c
Kristian Cho <krischo@umich.edu>
2021-08-26 01:26:42 -0400

    Added functionality to upload images to user profile

--- timbre-functions/functions/index.js
+++ timbre-functions/functions/index.js
@@ -3,13 +3,15 @@ const functions = require("firebase-functions");
 const app = require('express')();
 
 const { getAllPosts, postOnePost } = require('./handlers/posts');
-const { signup, login } = require('./handlers/users'); 
+const { signup, login, uploadImage, addUserDetails } = require('./handlers/users'); 
 
 const FBAuth = require('./util/FBAuth');
 
 // Post Routes
 app.get('/posts', getAllPosts);
 app.post('/post', FBAuth, postOnePost);
+app.post('/user/image', FBAuth, uploadImage);
+app.post('/user', FBAuth, addUserDetails);
 
 // Users Routes
 app.post('/signup', signup);
