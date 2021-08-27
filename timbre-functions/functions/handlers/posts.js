6d6aef42a0afd22666ff22af8a06fb15f8737b04
Kristian Cho <krischo@umich.edu>
2021-08-27 03:38:19 -0400

    Post comment functionality

--- timbre-functions/functions/handlers/posts.js
+++ timbre-functions/functions/handlers/posts.js
@@ -75,3 +75,31 @@ exports.getPost = (req, res) => {
             return res.status(500).json({ error: err.code });
         })
 };
+
+// Comment on post
+exports.commentOnPost = (req, res) => {
+    if(req.body.body.trim() === '') return res.status(400).json({ error: 'Cannot be empty'});
+
+    const newComment = {
+        body: req.body.body,
+        createdAt: new Date().toISOString(),
+        postId: req.params.postId,
+        userHandle: req.user.handle,
+        userImage: req.user.imageUrl
+    }
+
+    db.doc(`/posts/${req.params.postId}`).get()
+        .then((doc) => {
+            if(!doc.exists) {
+                return res.status(404).json({ error: 'Post not found' });
+            }
+            return db.collection('comments').add(newComment);
+        })
+        .then(() => {
+            return res.json(newComment);
+        })
+        .catch(err => {
+            console.error(err);
+            res.status(500).json({ error: 'Something went wrong' });
+        })
+};
